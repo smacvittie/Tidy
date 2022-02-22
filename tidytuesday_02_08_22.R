435a0ff0e0bdac0d99c4d6493049902afef3fee9
smacvittie <85651215+smacvittie@users.noreply.github.com>
2022-02-22 12:42:31 -0800

    updating from 2/8

--- tidytuesday_02_08_22.R
+++ tidytuesday_02_08_22.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(stringr)
+library(ggpmisc)
 ```
 
 ```{r}
@@ -58,7 +59,52 @@ View(breed_traits_mat)
 
 cor_mat <- cor(breed_traits_mat)
 View(cor_mat)
-heat_pal <- colorRampPalette(c("white", "lightblue", "darkblue"),)
+heat_pal <- colorRampPalette(c("white", "lightblue", "darkblue"))
 heatmap.2(cor_mat, trace= 'none', col = heat_pal(50000))
+
+
+################################
+# averaging ranks
+breed_rank_all$rank_avg <- rowMeans(breed_rank_all[,2:9],na.rm = T)
+
+# adding ranks to breed Traits
+breed_traits$rank_avg<- breed_rank_all$rank_avg
+
+# rename columns without spaces
+colnames(breed_traits) <- make.names(colnames(breed_traits), unique = T)
+
+# visualize unique for non-numeric variables
+coat_type<- unique(breed_traits$Coat.Type)
+coat_length<- unique(breed_traits$Coat.Length)
+
+# replace non-numerics with numeric values
+breed_traits_num <- within(breed_traits,Coat.Type <- factor(Coat.Type, labels = c(1:10)) )
+breed_traits_num <- within(breed_traits_num,Coat.Length <- factor(Coat.Length, labels = c(1:4)) )
+
+
+#scatterplots
+
+# SOURCE: https://groups.google.com/forum/#!topic/ggplot2/1TgH-kG5XMA for function
+
+lm_eqn <- function(breed_traits_num){
+  m <- lm(rank_avg ~ Affectionate.With.Family, breed_traits_num);
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(unname(coef(m)[1]), digits = 2),
+                        b = format(unname(coef(m)[2]), digits = 2),
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));
+}
+
+p1 <- ggplot(breed_traits_num, aes(x=Affectionate.With.Family,y=rank_avg))+
+  geom_point()+
+  geom_smooth(method = "lm",se=FALSE, color="black")+
+  geom_text(x = 1, y = 175, label = lm_eqn(breed_traits_num), parse = TRUE)+
+  theme_bw()
+p1
+  
+
+p2 <- ggplot(breed_traits_num, aes(x=Good.With.Young.Children ,y=rank_avg))+
+  geom_point()+
+  theme_classic()  
 
 
